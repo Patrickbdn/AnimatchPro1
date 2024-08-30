08598f2d4efda3c9f5ffb5eef13387a595a7c1af
Patrickbdn <patrick.bodin.pro@gmail.com>
2024-08-30 08:48:16 +0200

    maj navbar -> menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,57 +1,58 @@
-// Importation de la bibliothèque React
-import React from "react";
-
-// Importation du composant Link de react-router-dom pour la navigation
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-// Importation des styles CSS depuis le fichier NavBar.module.css
 import styles from "./NavBar.module.css";
 
-// Définition du composant fonctionnel NavBar
 export const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    // Élément de navigation avec la classe CSS 'navbar'
-    <nav className={styles.navbar}>
-      {/* Liste des éléments de navigation */}
-      <ul>
-        {/* Élément de liste pour le lien vers la page d'accueil */}
+    <nav className={`${styles.navbar} ${isOpen ? styles.open : ""}`}>
+      <button className={styles.menuButton} onClick={toggleMenu} type="button">
+        Menu
+      </button>
+      <ul className={styles.navList}>
         <li>
-          <Link to="/">Accueil</Link>
+          <Link to="/" onClick={toggleMenu}>
+            Accueil
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page des animaux domestiques */}
         <li>
-          <Link to="/animaux-domestiques">Animaux domestiques</Link>
+          <Link to="/animaux-domestiques" onClick={toggleMenu}>
+            Animaux domestiques
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page des animaux de la ferme */}
         <li>
-          <Link to="/animaux-de-la-ferme">Animaux de la ferme</Link>
+          <Link to="/animaux-de-la-ferme" onClick={toggleMenu}>
+            Animaux de la ferme
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page des autres NAC */}
         <li>
-          <Link to="/autres-nac">Autres NAC</Link>
+          <Link to="/autres-nac" onClick={toggleMenu}>
+            Autres NAC
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page de dépôt d'annonce */}
         <li>
-          <Link to="/deposer-une-annonce">Déposer une annonce</Link>
+          <Link to="/deposer-une-annonce" onClick={toggleMenu}>
+            Déposer une annonce
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page de création de compte */}
         <li>
-          <Link to="/creation-de-compte">Créer un compte</Link>
+          <Link to="/creation-de-compte" onClick={toggleMenu}>
+            Créer un compte
+          </Link>
         </li>
-
-        {/* Élément de liste pour le lien vers la page de connexion */}
         <li>
-          <Link to="/connexion">Connexion</Link>
+          <Link to="/connexion" onClick={toggleMenu}>
+            Connexion
+          </Link>
         </li>
       </ul>
     </nav>
   );
 };
 
-// Exportation du composant NavBar par défaut
 export default NavBar;
